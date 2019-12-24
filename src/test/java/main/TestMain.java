16afdf2579d185d6410194700f059756b9fd4390
ansvir <46724063+ansvir@users.noreply.github.com>
2019-12-24 03:09:16 +0300

    commit 15

--- src/test/java/main/TestMain.java
+++ src/test/java/main/TestMain.java
@@ -3,49 +3,44 @@ package main;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
+import part.two.chapter.one.TestChapterOne;
 import part.one.chapter.three.TestBoolMatrix;
 import part.one.chapter.three.TestStringMatrix;
 import part.one.chapter.two.TestChapterTwo;
-import part.two.chapter.one.TestChapterOne;
 
 public class TestMain {
+
+    private final static String PART="\u001B[43m";
+    private final static String CHAPTER="\u001B[34m";
+    private final static String DONE="\u001B[32m";
+    private final static String FAIL="\u001B[31m";
+    private final static String RESET="\u001B[0m";
+
+
     public static void main(String[] args) {
-        Result result = JUnitCore.runClasses(TestChapterTwo.class);
+        Result result=null;
+
+        result= JUnitCore.runClasses(TestChapterTwo.class);
+        System.out.println(PART+"Part one"+RESET);
+        toConsole(result, "ChapterTwo: task10");
+
+        result= JUnitCore.runClasses(TestBoolMatrix.class);
+        toConsole(result, "ChapterThree: BoolMatrix");
+
+        result= JUnitCore.runClasses(TestStringMatrix.class);
+        toConsole(result, "ChapterThree: StringMatrix");
+
+        result= JUnitCore.runClasses(TestChapterOne.class);
+        System.out.println(PART+"Part two"+RESET);
+        toConsole(result, "ChapterOne: task4");
+    }
+
+    private static void toConsole(Result result, String chapterAndTask) {
+
+        System.out.println(CHAPTER+chapterAndTask+RESET+" - "+(result.wasSuccessful()?DONE+"done"+RESET:FAIL+"fail"+RESET));
 
         for (Failure failure : result.getFailures()) {
             System.out.println(failure.toString());
         }
-
-        System.out.println("Part one");
-        System.out.println("ChapterTwo: task10");
-        System.out.println(result.wasSuccessful());
-
-        result = JUnitCore.runClasses(TestBoolMatrix.class);
-
-        for (Failure failure : result.getFailures()) {
-            System.out.println(failure.toString());
-        }
-
-        System.out.println("ChapterThree: BoolMatrix");
-        System.out.println(result.wasSuccessful());
-
-        result = JUnitCore.runClasses(TestStringMatrix.class);
-
-        for (Failure failure : result.getFailures()) {
-            System.out.println(failure.toString());
-        }
-
-        System.out.println("ChapterThree: StringMatrix");
-        System.out.println(result.wasSuccessful());
-
-        result = JUnitCore.runClasses(TestChapterOne.class);
-
-        for (Failure failure : result.getFailures()) {
-            System.out.println(failure.toString());
-        }
-
-        System.out.println("Part two");
-        System.out.println("ChapterOne: task4");
-        System.out.println(result.wasSuccessful());
     }
 }
